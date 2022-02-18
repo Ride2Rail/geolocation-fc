02c1bb56d510732b92f11ec26d4277c4eb0ce627
biotik <milan.straka92@gmail.com>
2022-02-18 13:39:51 +0100

    - changed the calling request to POST - added nominatim host and user as parameters

--- geolocation-fc.py
+++ geolocation-fc.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, request
 from r2r_offer_utils.advanced_logger import *
 
 from codes.geolocators import GeoLocationManager
@@ -12,10 +12,16 @@ config = ConfigLoader(LoggerFormatter(logger), service_name).config
 app = Flask(service_name)
 
 gm = GeoLocationManager(config)
+# curl --header 'Content-Type: application/json' \
+#        --request POST  \
+#        --data '{"request_id": "9d1cfc79-90e5-446b-8fbc-5e0c5ea9efa7" }' \
+#          http://localhost:5015/compute
 
 # curl -v -X GET "http://127.0.0.1:5015/9d1cfc79-90e5-446b-8fbc-5e0c5ea9efa7"
-@app.route('/<request_id>', methods=['GET'])
-def get_data(request_id):
+@app.route('/compute', methods=['POST'])
+def extract():
+    req_data = request.get_json()
+    request_id = req_data['request_id']
     data = gm.extract_cache_data(request_id)
     # if there was not anything written to cache
     if data is None:
@@ -23,7 +29,7 @@ def get_data(request_id):
     elif not data:
         logger.info("No location data was extracted from cache")
         return "No location data", 200
-    logger.info(f"Extracted the following data from chace: {data}")
+    logger.info(f"Extracted the following data from cache: {data}")
     cache_writing = gm.write_cache_data(request_id, data)
     if cache_writing:
         return "OK", 200
